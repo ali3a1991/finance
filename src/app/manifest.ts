0c906d84b8b99e4ca41d859c2b9f8d78b6e2c91d
Ali <ali.azimzadeh@artif.de>
2026-07-27 12:19:03 +0200

    Update icon sizes in manifest for correct display

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,12 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: "/icon-192.png",
-        sizes: "192x192",
+        sizes: "195x192",
         type: "image/png",
       },
       {
         src: "/icon-512.png",
-        sizes: "512x512",
+        sizes: "521x512",
         type: "image/png",
       },
     ],
